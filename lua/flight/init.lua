bf40bb9c47a49a975496ebeff0133fd96c77178a
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-01-27 13:54:28 +0800

    put all things into 1 file, remade init.lua, old files are in .lua.bak files

--- lua/flight/init.lua
+++ lua/flight/init.lua
@@ -10,13 +10,44 @@ NOTE: flow of execution
 
 local M = {}
 
----@param options? Options
-function M.setup(options)
-    local config = require "flight.config"
-    config.setup(options)
+M.default_config = {
+    mappings = {
+        f = "f",
+        F = "F",
+        t = "t",
+        T = "T",
+        [";"] = ";",
+        [","] = ",",
+    },
+    -- the key is the native bindings; the value is the new bindings of the new function
+    -- if they are the same, it will override the native bindings
+    -- you can keep the native bindings if you set the value to other keymaps
+    highlighted = 15, -- resuts to be highlighted
+    numbered = 9, -- results to be numbered (max 9 for now)
+}
 
-    -- create highlight group
-    vim.api.nvim_set_hl(0, "FlightTargets", { underline = true })
+---@type Options
+M.config = {}
+
+---@param u_options? Options
+function M.load_options(u_options)
+    M.config =
+        vim.tbl_deep_extend("force", {}, M.default_config, u_options or {})
+end
+
+---@param u_options? Options
+function M.setup(u_options)
+    -- load options
+    M.load_options(u_options)
+
+    -- create highlight groups
+    vim.api.nvim_set_hl(
+        0,
+        "FlightUnnumberedTargets",
+        { link = "FlightNumberedTargets", fg = "red" }
+    )
+    vim.api.nvim_set_hl(0, "FlightNumberedTargets", { underline = true })
+
     -- create mappings
 end
 
