9d96ec5b221bb382dde8469fa6519c4b750aab9c
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-02-21 19:58:25 +0800

    more mapping, enhanced M.map

--- lua/flight/init.lua
+++ lua/flight/init.lua
@@ -1,13 +1,13 @@
 --[[
- NOTE:
- flow of execution
+    NOTE:
+    flow of execution
      0. create config, highlights, mappings
      1. find the characters
      2. highlight the characters
      3. change the characters to numbers
      4. wait for user to enter the numebr
      5. move the cursor to the position
- plugin behaviour:
+    plugin behaviour:
      1. {f/F/t/T}{char} -> got to {default char} pos
      2. display numbers (2 to 9) (few secs)
      3. if {}{char}{num} then repeate from 2.
@@ -22,20 +22,27 @@ local config = require "flight.config"
 -- MAPPINGS --
 --------------
 function M.load_mappings()
-    M.map("n", config.default.mappings.f, "<cmd>lua print 'juiceDeLemon'<cr>", { desc = "Foward" })
+    -- stylua: ignore start
+    M.map(config.default.mappings.f, "<cmd>lua require'flight.jump'.f(false)<cr>", "Foward")
+    M.map(config.default.mappings.F, "<cmd>lua require'flight.jump'.F(false)<cr>", "Backward")
+    M.map(config.default.mappings.t, "<cmd>lua require'flight.jump'.t(false)<cr>", "Fowards Until")
+    M.map(config.default.mappings.T, "<cmd>lua require'flight.jump'.T(false)<cr>", "Backwards Until")
+    M.map(config.default.mappings.continue, "<cmd>lua require'flight.jump'.continue(false)<cr>", "Continue")
+    M.map(config.default.mappings.continue_reverse, "<cmd>lua require'flight.jump'.continue(true)<cr>", "Continue Reverse")
+    -- stylua: ignore end
 end
 
-function M.map(mode, key, rhs, opts)
+function M.map(key, rhs, opts)
     if key == "" then
         return
     end
 
-    opts = vim.tbl_deep_extend("force", { noremap = true }, opts or {})
+    opts = vim.tbl_deep_extend("force", { noremap = true }, { desc = opts } or {})
     if vim.fn.has "nvim-0.7" == 0 then
         opts.desc = nil
     end
 
-    vim.api.nvim_set_keymap(mode, key, rhs, opts)
+    vim.api.nvim_set_keymap("n", key, rhs, opts)
 end
 
 -----------
