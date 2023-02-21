0e10561303d47f2857a382cf86deba8e7277400b
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-02-21 18:59:46 +0800

    added state table and swapdirection func

--- lua/flight/utils.lua
+++ lua/flight/utils.lua
@@ -1,6 +1,23 @@
 local M = {}
 
-M.variants = { "f", "F", "t", "T", ";", "," }
+M.state = {
+    last_mode = "",
+    last_target = "",
+    is_highlighting = false,
+}
+
+function M.swap_direction(mode)
+    if mode == "f" then
+        return "F"
+    elseif mode == "F" then
+        return "f"
+    elseif mode == "t" then
+        return "T"
+    elseif mode == "T" then
+        return "t"
+    end
+    return ""
+end
 
 function M.is_in_array(value, array)
     for _, v in ipairs(array) do
