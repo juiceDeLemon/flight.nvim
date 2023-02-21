abeeb717c6c1d993736c5eca78700cfd0fb88901
juiceDeLemon <77475400+juiceDeLemon@users.noreply.github.com>
2023-02-21 19:59:16 +0800

    +multiline, clear_highlight, ignore_case *mappings for testing -> <leader>, name for [";"] is changed to continue & continue_reverse

--- lua/flight/config.lua
+++ lua/flight/config.lua
@@ -2,20 +2,21 @@ local M = {}
 
 M.default = {
     mappings = {
-        f = "f",
-        F = "F",
-        t = "t",
-        T = "T",
-        [";"] = ";",
-        [","] = ",",
+        f = "<leader><leader>f",
+        F = "<leader><leader>F",
+        t = "<leader><leader>t",
+        T = "<leader><leader>T",
+        continue = "<leader><leader>;", -- aka ";"
+        continue_reverse = "<leader><leader>,", -- aka ","
     },
-    -- the key is the native bindings; the value is the new bindings of the new function
-    -- if they are the same, it will override the native bindings
+    -- the key is the native bindings (can't get [";"] or [","] to work); the value is the new bindings of the new function
     -- you can keep the native bindings if you set the value to other keymaps
     -- empty string "" will disable the binding
     highlighted = 15, -- resuts to be highlighted
     numbered = 9, -- results to be numbered (max 9 for now)
-    clear_highlight = 10000, -- remove the number and highlight after how many ms
+    multiline = 3, -- how many lines do multiline consider, up and down each (not combined). set to 0 to disable multiline
+    clear_highlight = 2000, -- remove the number and highlight after how many ms
+    ignore_case = false, -- ignore case
 }
 
 M.result = {}
